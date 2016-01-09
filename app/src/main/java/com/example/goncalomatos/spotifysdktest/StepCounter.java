0cb9be52c124d89d29e5f31731b309927997dbac
Carlos Pinto <carlosnpinto2@gmail.com>
2016-01-09 16:46:35 +0000

    Changed stepCounter class to register stepDetector on creation and listen to stepDetector callbacks.

--- app/src/main/java/com/example/goncalomatos/spotifysdktest/StepCounter.java
+++ app/src/main/java/com/example/goncalomatos/spotifysdktest/StepCounter.java
@@ -1,11 +1,23 @@
 package com.example.goncalomatos.spotifysdktest;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
 public class StepCounter implements StepListener{
 
     private int steps;
+    private StepDetector stepDetector;
+    private Sensor mSensor;
+    private SensorManager mSensorManager;
 
-    public StepCounter() {
+    public StepCounter(Context context) {
         steps = 0;
+
+        mSensorManager = (SensorManager)context.getSystemService(context.SENSOR_SERVICE);
+        stepDetector = new StepDetector();
+        stepDetector.addStepListener(this);
+        registerStepDetector();
     }
 
     @Override
@@ -15,5 +27,13 @@ public class StepCounter implements StepListener{
 
     public int getSteps() {
         return steps;
+    }
+
+    private void registerStepDetector() {
+        mSensor = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(stepDetector,
+                mSensor,
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
 }
