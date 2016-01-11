abafb280463b341de465852316b51a1a4b153c64
Gonçalo Matos <goncalomatos3@gmail.com>
2016-01-11 00:39:17 +0000

    Change min accuracy to 15m

--- app/src/main/java/com/example/goncalomatos/spotifysdktest/GPSDetector.java
+++ app/src/main/java/com/example/goncalomatos/spotifysdktest/GPSDetector.java
@@ -23,7 +23,7 @@ public class GPSDetector implements LocationListener, GoogleApiClient.Connection
     private static final long INTERVAL = 15000;
     private static final long MIN_INTERVAL = 5000;
     private static final float MIN_DISTANCE = 2;
-    private static final int MIN_ACCURACY = 5;
+    private static final int MIN_ACCURACY = 15;
 
     private Location startLocation = null;
     private Location lastLocation = null;
